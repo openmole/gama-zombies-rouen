979ab4a275f0972049b1c348f389566c0341c6ec
chapinux <paul.chapron@gmail.com>
2017-03-01 10:59:17 +0700

    filtrer resultats selon le nombre replication

--- visualisation-with-R/simple_visualisation/visu_PSE.R
+++ visualisation-with-R/simple_visualisation/visu_PSE.R
@@ -4,31 +4,39 @@ library(maptools)
 library(scales)
 library(RColorBrewer)
 library(dplyr)
+library(RCurl)
 
 
 
+#pour choper directement le fichier résultat le plus récent
+#x <- getURL("https://raw.githubusercontent.com/openmole/gama-zombies-rouen/master/PSE_results/population19727.csv")
+#dfpse <- read.csv(text = x)
 
-dfpse <- read.csv("../../PSE_results/population5415.csv",header = T)
-head(dfpse)
 
+dfpse <- read.csv("../../PSE_results/population33033.csv",header = T)%>% 
+          filter(evolution.samples>30)
 
 # noms des dimensions à afficher dans le nuage de points 3D 
 dim_of_interest <- c("infection_distance", "max_infection_ratio",  "max_size")
 
 # label des points min et max des colonnes ci dessus 
-
-
-p3d<- plot_ly(dfpse, x = ~infection_distance, y = ~max_infection_ratio, z = ~max_size, 
-              mode="markers", 
-              type="scatter3d",
+p3d<- plot_ly(dfpse, x = ~max_infection_ratio, y = ~max_size, 
+              color=~proba_infection,
+              mode="markers",
+              type="scatter",
               hoverinfo="text",
               text = ~paste("", 'distInf:', round(infection_distance, digits = 3), 
                             '</br>max_infection_ratio:', round(max_infection_ratio, digits = 3),
                             '</br> max_size:', max_size),
-              marker = list(size = 2, 
+              marker = list(size = 7, 
                             opacity=0.5, 
                             line = list(width = 0.1, color="gray25") ))
 
 p3d
 
 
+names(dfpse)
+
+#tendances 2 à 2 
+
+plot(dfpse[,2:10])
